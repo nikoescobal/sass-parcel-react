6f67617042439a03ddf21886633e6c6bda4c5960
Nikolas Escobal <niko.escobal@gmail.com>
2022-07-06 18:25:34 +0800

    Remove stylelint

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,3 @@
-export function App() {
+export default function App() {
   return <h1>Hello world!</h1>;
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 // import ReactDOM from "react-dom";
-import * as ReactDOM from "react-dom/client";
-import { App } from "./App";
+import * as ReactDOM from 'react-dom/client';
+import { App } from './App.js';
 
-const app = document.getElementById("root");
+const app = document.getElementById('root');
 
 const root = ReactDOM.createRoot(app);
 root.render(<App />);
