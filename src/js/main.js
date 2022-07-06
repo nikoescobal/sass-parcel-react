e7bd318c22e325c4c3fbf017b6d7565adb56672e
Nikolas Escobal <niko.escobal@gmail.com>
2022-07-06 17:41:50 +0800

    Update

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// import ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
+import { App } from "./App";
+
+const app = document.getElementById("root");
+
+const root = ReactDOM.createRoot(app);
+root.render(<App />);
