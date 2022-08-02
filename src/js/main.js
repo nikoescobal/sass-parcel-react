fcefed1701ea56619ad1decf7a40804b23e60f61
Nikolas Escobal <niko.escobal@gmail.com>
2022-08-03 02:36:14 +0800

    Fix linters

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 import * as ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import App from './App';
 
 const app = document.getElementById('root');
