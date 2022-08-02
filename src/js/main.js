0a5f562d3e5393c523c99025114ae9eb25ee9830
Nikolas Escobal <niko.escobal@gmail.com>
2022-08-03 01:23:26 +0800

    Fix linter

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 // import ReactDOM from "react-dom";
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
