180fcc40f9ed5cf54b725cf8a9286f122eb82523
Nikolas Escobal <niko.escobal@gmail.com>
2022-09-13 17:41:42 +0800

    Delete main.js

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import * as ReactDOM from 'react-dom/client';
-import App from './App';
-
-const app = document.getElementById('root');
-
-const root = ReactDOM.createRoot(app);
-root.render(<App />);
